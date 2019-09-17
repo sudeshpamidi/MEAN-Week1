5cf99e100bd702181a6df33050b59be102b8ce77
Sudesh Pamidi <sudesh.pamidi@gmail.com>
2019-09-16 21:30:58 -0400

    Remember me feature completed

--- usermanagement/public/javascripts/login.js
+++ usermanagement/public/javascripts/login.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
 
+    var userName = $('#username');
+    var password = $('#password');
+    var rememberMe = $('#rememberMe');
+
     $(".alert").hide();
     $("#btnLogin").on('click', (e) => {
+
         event.preventDefault();
         if (!validator.validate("#frmLogin")) {
             return;
+        }
+        if (localStorargeSupported()) {
+            localStorage.setItem('persistedData', JSON.stringify({ "userName": userName.val(), "rememberMe": rememberMe.is(':checked') }));
         }
         $(".alert").show();
         let postData = $("#frmLogin").serialize();
@@ -23,4 +31,28 @@ $(document).ready(function() {
                     .html('Invalid Credentials');
             });
     });
+
+
+
+    function localStorargeSupported() {
+        if (typeof(Storage) !== "undefined") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // on page load
+    if (localStorargeSupported()) {
+        var persistedData = localStorage.getItem('persistedData');
+
+        if (persistedData !== null) {
+            var parsedData = JSON.parse(persistedData);
+            if (parsedData.rememberMe === true) {
+                userName.val(parsedData.userName);
+                rememberMe.prop('checked', true);
+                password.focus();
+            }
+        }
+    }
 });
